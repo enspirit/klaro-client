66cf4fc2babdc223d0136982b02bbea4fe3e8e32
Bernard Lambeau <blambeau@gmail.com>
2020-01-06 15:56:03 +0100

    Add support for handling attachments like inline images.

--- lib/klaro/client/resource/story.rb
+++ lib/klaro/client/resource/story.rb
@@ -2,29 +2,35 @@ module Klaro
   class Client
     class Story < Resource
 
-      def download_and_relocate_images(root_path, folder, client)
-        spec = handle_images(specification) do |url|
-          handle_image(url, root_path, folder, client)
+      def download_and_relocate_attachments(root_path, target_folder, client)
+        as = self.attachments.map do |attachment|
+          url = attachment["url"]
+          url += "?n=" + URI.encode_www_form_component(attachment["filename"]) unless url =~ /\?n=/
+          path = handle_image(url, root_path, target_folder, client)
+          attachment.merge("url" => path)
         end
-        get_new_story(spec)
+        self.class.new(self.to_h.merge(
+          attachments: as
+        ))
+      rescue => ex
+        puts ex.message
+        puts ex.backtrace.join("\n")
+        raise
+      end
+
+      def download_and_relocate_images(root_path, target_folder, client)
+        spec = self.specification.gsub(%r{!\[([^\]]*)\]\(([^\)]*)\)}) do
+          m = Regexp.last_match
+          label, url = m[1], m[2]
+          image_relative_path = handle_image(url, root_path, target_folder, client)
+          "![#{label}](#{image_relative_path})"
+        end
+        self.class.new(self.to_h.merge(
+          specification: spec
+        ))
       end
 
     private
-
-      def get_new_story(spec)
-        story = self.class.new(self.to_h)
-        story.specification = spec
-        story
-      end
-
-      def handle_images(specification, &block)
-        specification.gsub(%r{!\[([^\]]*)\]\(([^\)]*)\)}) do
-          m = Regexp.last_match
-          label, url = m[1], m[2]
-          image_relative_path = block.call(url)
-          "![#{label}](#{image_relative_path})"
-        end
-      end
 
       def handle_image(url, root_path, folder, client)
         image_path = get_image_path(url, folder)
@@ -34,13 +40,14 @@ module Klaro
 
       def get_image_path(url, folder)
         file_name = URI.parse(url).query.gsub('n=', '')
+        file_name = URI.decode_www_form_component(file_name)
         folder/"#{self['card-kind']}/#{identifier}/#{file_name}"
       end
 
-      def download_image(url, image_path, client)
-        image_path.parent.mkdir_p unless image_path.parent.exists?
+      def download_image(url, target_file, client)
+        target_file.parent.mkdir_p unless target_file.parent.exists?
         img = client.request.get("#{url}", true)
-        image_path.write(img)
+        target_file.write(img)
       end
 
     end
