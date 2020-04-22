70249658e5e020d0518867a88bf1ed1e5f4830f4
Bernard Lambeau <blambeau@gmail.com>
2020-04-22 07:38:06 +0200

    Bump to 0.5.0 and release.

--- lib/klaro/client/version.rb
+++ lib/klaro/client/version.rb
@@ -2,8 +2,8 @@ module Klaro
   class Client
     module Version
       MAJOR = 0
-      MINOR = 4
-      TINY  = 5
+      MINOR = 5
+      TINY  = 0
     end
     VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::TINY}"
   end # class Client
